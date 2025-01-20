61155108501290c676390d5c42200ed1a2774f46
DenisKomov <143321805+DenisKomov@users.noreply.github.com>
2025-01-20 18:21:33 +0300

    commit 2
    commit 2

--- src/js/_test_/Character.test.js
+++ src/js/_test_/Character.test.js
@@ -8,6 +8,26 @@ test('Получим объект Character', () => {
   expect(description).toEqual(result);
 });
 
+test('Повышаем уровень персонажа', () => {
+  const description = new Character('warrior');
+  description.health = 50;
+  description.attack = 10;
+  description.defense = 10;
+  description.levelUp();
+  const result = {
+    name: 'warrior', health: 100, level: 2, attack: 12, defense: 12,
+  };
+  expect(description).toEqual(result);
+});
+
+test('нельзя повысить левел умершего', () => {
+  function test() {
+    const description = new Character('warrior');
+    description.health = 0;
+    return description.levelUp();
+  }
+  expect(test).toThrow('нельзя повысить левел умершего');
+});
 
 test('Получим ошибку имени', () => {
   const description = () => new Character('B');
